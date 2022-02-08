73d40d69f058c0e4aff70d4ebc1cde4047daa02d
L1ghtingBolt <82386159+L1ghtingBolt@users.noreply.github.com>
2022-02-08 17:25:29 +0100

    Fixed article size on mobile.

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -7,7 +7,7 @@
 </script>
 
 
-<article class="mb-10 border-[1px] p-10 rounded-lg border-gray-400 dark:border-gray-300 min-w-[420px] max-w-max">
+<article class="mb-10 border-[1px] p-10 rounded-lg border-gray-400 dark:border-gray-300 min-w-[420px]">
     <h2 class="text-gray-500">{date}</h2>
 	<a class="hover:underline {nsfw ? 'decoration-red-700' : 'decoration-orange-500'} font-bold text-2xl" href={`${url}`}>{title}</a>
 	<p class="text-gray-600">{content} </p>
